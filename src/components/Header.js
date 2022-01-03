58ad99d7332abbb1ab362d3efc8e6dd863c3e0dd
Alex Gan <alex335616249@gmail.com>
2022-01-03 08:01:23 -0500

    add buttons

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from "./Button"
+
+const Header = () => {
+    return (
+        <header>
+           <h1>Task Tracker</h1> 
+            <Button color='blue' text='Button 1'/>
+            <Button color='red' text='oof'/>
+            <Button color='green' text='royale'/>
+            <Button color='green' text='royale'/>
+        </header>
+    )
+}
+
+export default Header
